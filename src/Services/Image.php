caf4c0a267cdb6788cee9b32590e42efb68b960b
Grandel Cuvos <graandelc@gmail.com>
2017-11-30 11:02:38 -0600

    Hooking up the ImageService to the Shopify Image API (#26)

--- src/Services/Image.php
+++ src/Services/Image.php
@@ -2,9 +2,78 @@
 
 namespace BoldApps\ShopifyToolkit\Services;
 
+use BoldApps\ShopifyToolkit\Models\Product as ShopifyProduct;
+use BoldApps\ShopifyToolkit\Models\Image as ShopifyImage;
+use Illuminate\Support\Collection;
+
 /**
  * Class Image.
  */
 class Image extends Base
 {
+    /**
+     * @param \BoldApps\ShopifyToolkit\Models\Product $product
+     * @param \BoldApps\ShopifyToolkit\Models\Image $image
+     *
+     * @return object
+     */
+    public function create(ShopifyProduct $product, ShopifyImage $image)
+    {
+        $serializedModel = ['image' => $this->serializeModel($image)];
+
+        $raw = $this->client->post("admin/products/{$product->getId()}/images.json", [], $serializedModel);
+
+        return $this->unserializeModel($raw['image'], ShopifyImage::class);
+    }
+
+    /**
+     * @param $array
+     *
+     * @return object
+     */
+    public function createFromArray($array)
+    {
+        return $this->unserializeModel($array, ShopifyImage::class);
+    }
+
+    /**
+     * @param \BoldApps\ShopifyToolkit\Models\Product $product
+     * @param array $params
+     * @return Collection
+     */
+    public function getAll(ShopifyProduct $product, $params = [])
+    {
+        $raw = $this->client->get("admin/products/{$product->getId()}/images.json", $params);
+
+        $images = array_map(function($image) {
+            return $this->unserializeModel($image, ShopifyImage::class);
+        }, $raw['images']);
+
+        return new Collection($images);
+    }
+
+    /**
+     * @param \BoldApps\ShopifyToolkit\Models\Product $product
+     * @param $id
+     *
+     * @return ShopifyImage
+     */
+    public function getById(ShopifyProduct $product, $id)
+    {
+        $raw = $this->client->get("admin/products/{$product->getId()}/images/{$id}");
+
+        return $this->unserializeModel($raw['images'], ShopifyImage::class);
+    }
+
+    /**
+     * @param \BoldApps\ShopifyToolkit\Models\Product $product
+     * @param \BoldApps\ShopifyToolkit\Models\Image $image
+     *
+     * @return array
+     */
+    public function delete(ShopifyProduct $product, ShopifyImage $image)
+    {
+        return $this->client->delete("admin/products/{$product->getId()}/images/{$image->getId()}.json");
+    }
+
 }
