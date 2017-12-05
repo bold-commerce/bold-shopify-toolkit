57f75a4646f9fa9a5892155522a0a5734f664b84
Brianna <brianna.mansell@boldcommerce.com>
2017-12-05 12:56:29 -0600

    Feature for Shopify Price Rules API
    - Added model, service, and test for Shopify's price rules API
    - Based model off of and took test data from help.shopify.com/api/reference/pricerule
       - Filled in missing test data with real shop json data from hitting those endpoints
    - `value` is actually a string in the json returned from the API, not an int/float like the properties table says

---
+++ src/Services/PriceRule.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace BoldApps\ShopifyToolkit\Services;
+
+use BoldApps\ShopifyToolkit\Models\PriceRule as ShopifyPriceRule;
+use Illuminate\Support\Collection;
+
+class PriceRule extends CollectionEntity
+{
+    /**
+     * @param $id
+     *
+     * @return ShopifyPriceRule | object
+     */
+    public function getById($id)
+    {
+        $raw = $this->client->get("admin/price_rules/$id.json");
+
+        return $this->unserializeModel($raw['price_rule'], ShopifyPriceRule::class);
+    }
+
+    /**
+     * @param int   $page
+     * @param int   $limit
+     * @param array $filter
+     *
+     * @return Collection
+     */
+    public function getAll($page = 1, $limit = 50, $filter = [])
+    {
+        $raw = $this->client->get('admin/price_rules.json', array_merge(['page' => $page, 'limit' => $limit], $filter));
+
+        $priceRules = array_map(function ($priceRule) {
+            return $this->unserializeModel($priceRule, ShopifyPriceRule::class);
+        }, $raw['price_rules']);
+
+        return new Collection($priceRules);
+    }
+
+    /**
+     * @param array $params
+     *
+     * @return Collection
+     */
+    public function getByParams($params)
+    {
+        $raw = $this->client->get('admin/price_rules.json', $params);
+
+        $priceRules = array_map(function ($priceRule) {
+            return $this->unserializeModel($priceRule, ShopifyPriceRule::class);
+        }, $raw['price_rules']);
+
+        return new Collection($priceRules);
+    }
+
+    /**
+     * @param ShopifyPriceRule $priceRule
+     *
+     * @return ShopifyPriceRule | object
+     */
+    public function create(ShopifyPriceRule $priceRule)
+    {
+        $serializedModel = ['price_rule' => array_merge($this->serializeModel($priceRule))];
+
+        $raw = $this->client->post('admin/price_rules.json', [], $serializedModel);
+
+        return $this->unserializeModel($raw['price_rule'], ShopifyPriceRule::class);
+    }
+
+    /**
+     * @param ShopifyPriceRule $priceRule
+     *
+     * @return ShopifyPriceRule | object
+     */
+    public function update(ShopifyPriceRule $priceRule)
+    {
+        $serializedModel = ['price_rule' => $this->serializeModel($priceRule)];
+
+        $raw = $this->client->put("admin/price_rules/{$priceRule->getId()}.json", [], $serializedModel);
+
+        return $this->unserializeModel($raw['price_rule'], ShopifyPriceRule::class);
+    }
+
+    /**
+     * @param ShopifyPriceRule $priceRule
+     *
+     * @return ShopifyPriceRule | object
+     */
+    public function delete(ShopifyPriceRule $priceRule)
+    {
+        return $this->client->delete("admin/price_rules/{$priceRule->getId()}.json");
+    }
+}
