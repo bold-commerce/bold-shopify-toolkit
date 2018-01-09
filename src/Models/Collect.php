d8ccd940f9619ffcbd4b6e734002ddba87e09d95
Brianna Mansell <mansellb@myumanitoba.ca>
2018-01-09 11:44:42 -0600

    Add support for collects endpoints (#39)
    - This is so that we can easily retrieve product IDs via their collection ID
    
    Changes
    - Added functions for create, get, delete, and count endpoints
    - Added a test for serializing and unserializing a collect model
    - Reference can be found at https://help.shopify.com/api/reference/collect

---
+++ src/Models/Collect.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace BoldApps\ShopifyToolkit\Models;
+
+use BoldApps\ShopifyToolkit\Contracts\Serializeable;
+
+class Collect implements Serializeable
+{
+    /** @var int */
+    protected $id;
+
+    /** @var int */
+    protected $collectionId;
+
+    /** @var int */
+    protected $productId;
+
+    /** @var bool */
+    protected $featured;
+
+    /** @var int */
+    protected $position;
+
+    /** @var string */
+    protected $sortValue;
+
+    /** @var string */
+    protected $createdAt;
+
+    /** @var string */
+    protected $updatedAt;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCollectionId()
+    {
+        return $this->collectionId;
+    }
+
+    /**
+     * @param int $collectionId
+     */
+    public function setCollectionId($collectionId)
+    {
+        $this->collectionId = $collectionId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getProductId()
+    {
+        return $this->productId;
+    }
+
+    /**
+     * @param int $productId
+     */
+    public function setProductId($productId)
+    {
+        $this->productId = $productId;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getFeatured()
+    {
+        return $this->featured;
+    }
+
+    /**
+     * @param bool $featured
+     */
+    public function setFeatured($featured)
+    {
+        $this->featured = $featured;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    /**
+     * @param int $position
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSortValue()
+    {
+        return $this->sortValue;
+    }
+
+    /**
+     * @param string $sortValue
+     */
+    public function setSortValue($sortValue)
+    {
+        $this->sortValue = $sortValue;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param string $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param string $updatedAt
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+}
---
+++ src/Services/Collect.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace BoldApps\ShopifyToolkit\Services;
+
+use BoldApps\ShopifyToolkit\Models\Collect as ShopifyCollect;
+use BoldApps\ShopifyToolkit\Services\Client as ShopifyClient;
+use Illuminate\Support\Collection;
+
+class Collect extends Base
+{
+    /**
+     * Collect constructor.
+     *
+     * @param ShopifyClient $client
+     */
+    public function __construct(ShopifyClient $client)
+    {
+        parent::__construct($client);
+    }
+
+    /**
+     * @param ShopifyCollect $collect
+     *
+     * @return ShopifyCollect | object
+     */
+    public function create(ShopifyCollect $collect)
+    {
+        $serializedModel = ['collect' => array_merge($this->serializeModel($collect))];
+
+        $raw = $this->client->post('admin/collects.json', [], $serializedModel);
+
+        return $this->unserializeModel($raw['collect'], ShopifyCollect::class);
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return ShopifyCollect | object
+     */
+    public function getById($id)
+    {
+        $raw = $this->client->get("admin/collects/$id.json");
+
+        return $this->unserializeModel($raw['collect'], ShopifyCollect::class);
+    }
+
+    /**
+     * @param array $params
+     *
+     * @return Collection
+     */
+    public function getByParams($params = [])
+    {
+        $raw = $this->client->get('admin/collects.json', $params);
+
+        $priceRules = array_map(function ($collect) {
+            return $this->unserializeModel($collect, ShopifyCollect::class);
+        }, $raw['collects']);
+
+        return new Collection($priceRules);
+    }
+
+    /**
+     * @param array $params
+     *
+     * @return Collection
+     */
+    public function count($params = [])
+    {
+        $raw = $this->client->get('admin/collects/count.json', $params);
+
+        return $raw['count'];
+    }
+
+    /**
+     * @param ShopifyCollect $collect
+     *
+     * @return ShopifyCollect | array
+     */
+    public function delete(ShopifyCollect $collect)
+    {
+        return $this->client->delete("admin/collects/{$collect->getId()}.json");
+    }
+
+    /**
+     * @param $array
+     *
+     * @return object
+     */
+    public function createFromArray($array)
+    {
+        return $this->unserializeModel($array, ShopifyCollect::class);
+    }
+}
