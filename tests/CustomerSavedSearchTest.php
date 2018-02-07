8f46bc702bb0b037c0f2634949a855b14575f11b
Brianna Mansell <mansellb@myumanitoba.ca>
2018-02-07 13:55:57 -0600

    Add customers endpoint to customer saved searches (#45)
    - Added dates to the customer saved search model
    - Added function to customer saved search service that grabs all customers that apply to the customer saved search ID
      - https://help.shopify.com/api/reference/customersavedsearch#other
    - Added tests for customer and customer saved search to ensure accurate param types
    - Added createFromArray function to customer service for easier testing

---
+++ tests/CustomerSavedSearchTest.php
@@ -0,0 +1,75 @@
+<?php
+
+use BoldApps\ShopifyToolkit\Services\Client;
+use BoldApps\ShopifyToolkit\Models\CustomerSavedSearch as ShopifyCustomerSavedSearch;
+use BoldApps\ShopifyToolkit\Services\CustomerSavedSearch as CustomerSavedSearchService;
+
+class CustomerSavedSearchTest extends \PHPUnit\Framework\TestCase
+{
+    /** @var CustomerSavedSearchService */
+    private $customerSavedSearchService;
+
+    protected function setUp()
+    {
+        /** @var Client $client */
+        $client = $this->createMock(Client::class);
+        $this->customerSavedSearchService = new CustomerSavedSearchService($client);
+    }
+
+    /**
+     * @test
+     */
+    public function ShopifyCustomerSavedSearchSerializesProperly()
+    {
+        $customerSavedSearchEntity = $this->createCustomerSavedSearchEntity();
+
+        $expected = $this->getCustomerSavedSearchArray();
+        $actual = $this->customerSavedSearchService->serializeModel($customerSavedSearchEntity);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function ShopifyCustomerSavedSearchDeserializesProperly()
+    {
+        $customerSavedSearchJson = $this->getCustomerSavedSearchJson();
+        $jsonArray = (array)json_decode($customerSavedSearchJson, true);
+
+        $expected = $this->createCustomerSavedSearchEntity();
+        $actual = $this->customerSavedSearchService->unserializeModel($jsonArray, ShopifyCustomerSavedSearch::class);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    private function createCustomerSavedSearchEntity()
+    {
+        /** @var ShopifyCustomerSavedSearch $customerSavedSearchEntity */
+        $customerSavedSearchEntity = $this->customerSavedSearchService->createFromArray($this->getCustomerSavedSearchArray());
+
+        return $customerSavedSearchEntity;
+    }
+
+    private function getCustomerSavedSearchJson()
+    {
+        return '{
+            "id": 1862334091,
+            "name": "Abandoned checkouts",
+            "created_at": "2017-05-25T10:02:43-05:00",
+            "updated_at": "2017-05-25T10:02:43-05:00",
+            "query": "last_abandoned_order_date:last_month"
+        }';
+    }
+
+    private function getCustomerSavedSearchArray()
+    {
+        return [
+            "id" => 1862334091,
+            "name" => "Abandoned checkouts",
+            "created_at" => "2017-05-25T10:02:43-05:00",
+            "updated_at" => "2017-05-25T10:02:43-05:00",
+            "query" => "last_abandoned_order_date:last_month",
+        ];
+    }
+}
