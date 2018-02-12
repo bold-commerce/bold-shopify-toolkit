5bdb6dab5b8505ebc444aed119977821ef642fb4
Brianna Mansell <mansellb@myumanitoba.ca>
2018-02-12 16:02:28 -0600

    Add a service for Shopify Country API (#47)
    - Functions for create, update, delete, getAll, getByCountryId, count, etc
    - Updates to the model to add setters for properties, as well as new tax name prop
      - It's not listed as a property on the page but it is returned from hitting the endpoint
      - https://help.shopify.com/api/reference/country#index
    - Added test for serializing/unserializing countries to ensure property types

--- src/Models/Country.php
+++ src/Models/Country.php
@@ -6,31 +6,23 @@ use BoldApps\ShopifyToolkit\Contracts\Serializeable;
 
 class Country implements Serializeable
 {
-    /**
-     * @var int
-     */
+    /** @var int */
     protected $id;
 
-    /**
-     * @var string
-     */
+    /** @var string */
     protected $code;
 
-    /**
-     * @var string
-     */
+    /** @var string */
     protected $name;
 
-    /**
-     * @var array
-     */
+    /** @var array */
     protected $provinces;
 
-    /**
-     * @var float
-     */
+    /** @var float */
     protected $tax;
 
+    /** @var string */
+    protected $taxName;
 
     /**
      * @return int
@@ -40,6 +32,13 @@ class Country implements Serializeable
         return $this->id;
     }
 
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
 
     /**
      * @return string
@@ -49,6 +48,13 @@ class Country implements Serializeable
         return $this->code;
     }
 
+    /**
+     * @param string $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
 
     /**
      * @return string
@@ -58,6 +64,13 @@ class Country implements Serializeable
         return $this->name;
     }
 
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
 
     /**
      * @return array
@@ -67,6 +80,13 @@ class Country implements Serializeable
         return $this->provinces;
     }
 
+    /**
+     * @param array $provinces
+     */
+    public function setProvinces($provinces)
+    {
+        $this->provinces = $provinces;
+    }
 
     /**
      * @return float
@@ -76,4 +96,27 @@ class Country implements Serializeable
         return $this->tax;
     }
 
+    /**
+     * @param float $tax
+     */
+    public function setTax($tax)
+    {
+        $this->tax = $tax;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTaxName()
+    {
+        return $this->taxName;
+    }
+
+    /**
+     * @param string $taxName
+     */
+    public function setTaxName($taxName)
+    {
+        $this->taxName = $taxName;
+    }
 }
