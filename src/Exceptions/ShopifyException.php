d90f9eb7b4f3d8c460e1e69b20f1b4a9b0d253b8
ben.penner <ben.penner@boldcommerce.com>
2020-12-07 13:13:15 -0600

    Adding responses to ShopifyExceptions

--- src/Exceptions/ShopifyException.php
+++ src/Exceptions/ShopifyException.php
@@ -4,4 +4,25 @@ namespace BoldApps\ShopifyToolkit\Exceptions;
 
 class ShopifyException extends \Exception
 {
+    private $response;
+
+    /**
+     * @return mixed
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    /**
+     * @param mixed $response
+     *
+     * @return ShopifyException
+     */
+    public function setResponse($response)
+    {
+        $this->response = $response;
+
+        return $this;
+    }
 }
