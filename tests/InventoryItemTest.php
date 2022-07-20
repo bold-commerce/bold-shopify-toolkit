e62be991f98c04d2328e2bd48d9881e98d869bf8
Navdeep Singh Sidhu <navdeep.sidhu@boldcommerce.com>
2022-07-20 15:52:10 -0500

    Added Inventory Item Model and Service * Added Model and Service for Inventory Item (https://shopify.dev/api/admin-rest/2022-04/resources/inventoryitem). * Added Unit test for the new service

---
+++ tests/InventoryItemTest.php
@@ -0,0 +1,77 @@
+<?php
+
+use BoldApps\ShopifyToolkit\Services\Client;
+use BoldApps\ShopifyToolkit\Models\InventoryItem as ShopifyInventoryItem;
+use BoldApps\ShopifyToolkit\Services\InventoryItem as InventoryItemService;
+
+class InventoryItemTest extends \PHPUnit\Framework\TestCase
+{
+    /** @var InventoryItemService */
+    private $inventoryItemService;
+
+    protected function setUp()
+    {
+        /** @var Client $client */
+        $client = $this->createMock(Client::class);
+        $this->inventoryItemService = new InventoryItemService($client);
+    }
+
+    /**
+     * @test
+     */
+    public function ShopifyInventoryItemSerializesProperly()
+    {
+        $inventoryLevelEntity = $this->inventoryItemService->createFromArray($this->getInventoryItemArray());
+
+        $expected = $this->getInventoryItemArray();
+        $actual = $this->inventoryItemService->serializeModel($inventoryLevelEntity);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function ShopifyInventoryItemDeserializesProperly()
+    {
+        $inventoryLevelJson = $this->getInventoryItemJson();
+        $jsonArray = (array) json_decode($inventoryLevelJson, true);
+
+        $expected = $this->inventoryItemService->createFromArray($this->getInventoryItemArray());
+        $actual = $this->inventoryItemService->unserializeModel($jsonArray, ShopifyInventoryItem::class);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    private function getInventoryItemJson()
+    {
+        return '{
+            "id": 808950810,
+            "sku": "IPOD2008PINK",
+            "created_at": "2022-04-05T13:05:24-04:00",
+            "updated_at": "2022-04-05T13:05:24-04:00",
+            "requires_shipping": true,
+            "cost": "25.00",
+            "country_code_of_origin": null,
+            "province_code_of_origin": null,
+            "harmonized_system_code": null,
+            "tracked": true,
+            "country_harmonized_system_codes": [],
+            "admin_graphql_api_id": "gid://shopify/InventoryItem/808950810"
+          }';
+    }
+
+    private function getInventoryItemArray()
+    {
+        return [
+            "id"=> 808950810,
+            "sku"=> "IPOD2008PINK",
+            "created_at"=> "2022-04-05T13:05:24-04:00",
+            "updated_at"=> "2022-04-05T13:05:24-04:00",
+            "requires_shipping"=> true,
+            "cost"=> "25.00",
+            "tracked"=> true,
+            "country_harmonized_system_codes"=> [],
+        ];
+    }
+}
