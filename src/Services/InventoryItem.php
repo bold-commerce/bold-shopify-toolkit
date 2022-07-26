060b21b66ffb866325da40f091b921488012f1d2
Navdeep Singh Sidhu <deepsidhu1313@users.noreply.github.com>
2022-07-26 11:27:36 -0500

    Added Inventory Item Model and Service (#149)
    * Added Model and Service for Inventory Item (https://shopify.dev/api/admin-rest/2022-04/resources/inventoryitem).
    * Added Unit test for the new service

---
+++ src/Services/InventoryItem.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace BoldApps\ShopifyToolkit\Services;
+
+use BoldApps\ShopifyToolkit\Models\InventoryItem as ShopifyInventoryItem;
+use Illuminate\Support\Collection;
+
+class InventoryItem extends CollectionEntity
+{
+    /**
+     * @param $array
+     *
+     * @return object
+     */
+    public function createFromArray($array)
+    {
+        return $this->unserializeModel($array, ShopifyInventoryItem::class);
+    }
+
+    /**
+     * @param       $id
+     * @param array $filter
+     *
+     * @return ShopifyInventoryItem | object
+     */
+    public function getById($id, $filter = [])
+    {
+        $raw = $this->client->get("{$this->getApiBasePath()}/inventory_items/$id.json", $filter);
+
+        return $this->unserializeModel($raw['inventory_item'], ShopifyInventoryItem::class);
+    }
+
+    /**
+     * @param array $params
+     *
+     * @return Collection
+     */
+    public function getByParams($params)
+    {
+        $raw = $this->client->get("{$this->getApiBasePath()}/inventory_items.json", $params);
+
+        $inventoryItems = array_map(function ($inventoryItem) {
+            return $this->unserializeModel($inventoryItem, ShopifyInventoryItem::class);
+        }, $raw['inventory_items']);
+
+        return new Collection($inventoryItems);
+    }
+
+    /**
+     * @return ShopifyInventoryItem | object
+     */
+    public function update(ShopifyInventoryItem $inventoryItem)
+    {
+        $serializedModel = ['inventory_item' => $this->serializeModel($inventoryItem)];
+        $raw = $this->client->put("{$this->getApiBasePath()}/inventory_items/{$inventoryItem->getId()}.json", [], $serializedModel);
+        return $this->unserializeModel($raw['inventory_item'], ShopifyInventoryItem::class);
+    }
+}
