2197630d0405bee45c8e6542983e89d2ddb6138a
Navdeep Singh Sidhu <deepsidhu1313@users.noreply.github.com>
2023-03-06 16:53:42 -0600

    Updated default API version to `2023-01` (#154)
    Updating default API version to `2023-01` to avoid deprecated calls when version is not specified. No breaking changes were observed. A separate MR will be created for changes related to FullfillmentOrder changes
    
    Co-authored-by: Navdeep Singh Sidhu <navdeep.sidhu@boldcommerce.com>

--- src/Services/Base.php
+++ src/Services/Base.php
@@ -9,7 +9,7 @@ abstract class Base
 {
     public const BASE_API_PATH = 'admin/api/%s';
 
-    public const DEFAULT_API_VERSION = '2021-07';
+    public const DEFAULT_API_VERSION = '2023-01';
 
     /** @var string */
     protected $shopifyApiVersion = self::DEFAULT_API_VERSION;
