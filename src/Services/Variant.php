3ae14d8eab7ade563dc5dfa7e15479573e78116f
Navdeep Singh Sidhu <navdeep.sidhu@boldcommerce.com>
2025-05-26 16:07:03 -0500

    Deprecate Products and Variants * Deprecate Products and Variants * removed functions which were using legacy shopify endpoints * Updated default api version to 2025-01

--- src/Services/Variant.php
+++ src/Services/Variant.php
@@ -9,17 +9,7 @@ use Illuminate\Support\Collection;
 
 class Variant extends Base
 {
-    /**
-     * @return object
-     */
-    public function create(ShopifyProduct $product, ShopifyVariant $variant)
-    {
-        $serializedModel = $this->serializeVariantCreateUpdate($variant);
 
-        $raw = $this->client->post("{$this->getApiBasePath()}/products/{$product->getId()}/variants.json", [], $serializedModel);
-
-        return $this->unserializeModel($raw['variant'], ShopifyVariant::class);
-    }
 
     /**
      * @param array $array
@@ -31,108 +21,6 @@ class Variant extends Base
         return $this->unserializeModel($array, ShopifyVariant::class);
     }
 
-    /**
-     * @return ShopifyVariant
-     */
-    public function getById($id)
-    {
-        $raw = $this->client->get("{$this->getApiBasePath()}/variants/$id.json");
-
-        return $this->unserializeModel($raw['variant'], ShopifyVariant::class);
-    }
-
-    /**
-     * @param int   $productId
-     * @param array $filter
-     *
-     * @return Collection
-     */
-    public function getAllByProductId($productId, $filter = [])
-    {
-        $raw = $this->client->get("{$this->getApiBasePath()}/products/$productId/variants.json", $filter);
-
-        $variants = array_map(function ($variant) {
-            return $this->unserializeModel($variant, ShopifyVariant::class);
-        }, $raw['variants']);
-
-        return new Collection($variants);
-    }
-
-    /**
-     * @return object
-     */
-    public function update(ShopifyVariant $variant)
-    {
-        $serializedModel = $this->serializeVariantCreateUpdate($variant);
-
-        $raw = $this->client->put("{$this->getApiBasePath()}/variants/{$variant->getId()}.json", [], $serializedModel);
-
-        return $this->unserializeModel($raw['variant'], ShopifyVariant::class);
-    }
-
-    /**
-     * @return object
-     */
-    public function delete(ShopifyProduct $product, ShopifyVariant $variant)
-    {
-        return $this->client->delete("{$this->getApiBasePath()}/products/{$product->getId()}/variants/{$variant->getId()}.json");
-    }
-
-    /**
-     * @return Collection
-     **/
-    public function createMetafield(ShopifyVariant $variant, ShopifyMetafield $metafield)
-    {
-        $serializedModel = ['metafield' => array_merge($this->serializeModel($metafield))];
-
-        $raw = $this->client->post("{$this->getApiBasePath()}/variants/{$variant->getId()}/metafields.json", [], $serializedModel);
-
-        return $this->unserializeModel($raw['metafield'], ShopifyMetafield::class);
-    }
-
-    /**
-     * @return Collection
-     */
-    public function updateMetafield(ShopifyVariant $variant, ShopifyMetafield $metafield)
-    {
-        $serializedModel = ['metafield' => array_merge($this->serializeModel($metafield))];
-
-        $raw = $this->client->put("{$this->getApiBasePath()}/variants/{$variant->getId()}/metafields/{$metafield->getId()}.json", [], $serializedModel);
-
-        return $this->unserializeModel($raw['metafield'], ShopifyMetafield::class);
-    }
-
-    /**
-     * @param array $params
-     *
-     * @return Collection
-     */
-    public function getMetafields(ShopifyVariant $variant, $params = [])
-    {
-        $raw = $this->client->get("{$this->getApiBasePath()}/variants/{$variant->getId()}/metafields.json", $params);
-
-        $metafields = array_map(function ($metafield) {
-            return $this->unserializeModel($metafield, ShopifyMetafield::class);
-        }, $raw['metafields']);
-
-        return new Collection($metafields);
-    }
-
-    /**
-     * @return Collection
-     */
-    public function deleteMetafield(ShopifyVariant $variant, ShopifyMetafield $metafield)
-    {
-        return $this->client->delete("{$this->getApiBasePath()}/variants/{$variant->getId()}/metafields/{$metafield->getId()}.json");
-    }
-
-    /**
-     * @return Collection
-     */
-    public function deleteMetafieldById(ShopifyMetafield $metafield)
-    {
-        return $this->client->delete("{$this->getApiBasePath()}/metafields/{$metafield->getId()}.json");
-    }
 
     /**
      * @param ShopifyVariant $variant
